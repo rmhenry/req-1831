8865eca9038deb91450380c5e07477098f392386
rmhenry <reeselightning83@gmail.com>
2018-05-30 14:50:18 -0500

    commented out problematic code to get back to a working version

--- public/company_table.js
+++ public/company_table.js
@@ -32,22 +32,22 @@
 
 var section = document.querySelector('section');
 
-//var requestURL = 'http://localhost:8080/api/1/companies';
-//var request = new XMLHttpRequest();
-//request.open('GET', requestURL);
-//request.responseType = 'json';
-//request.send();
+var requestURL = 'http://localhost:8080/api/1/companies';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
 
-//request.onload = function () {
-//    var oldCompanies = request.response;
-//    BuildCompanyTable(oldCompanies);
-//    DisplayCompanyData(oldCompanies);
-//}
-
-$(document).ready(function () {
-    var oldCompanies = $.getJSON('http://localhost:8080/api/1/companies');
+request.onload = function () {
+    var oldCompanies = request.response;
     BuildCompanyTable(oldCompanies);
-});
+    DisplayCompanyData(oldCompanies);
+}
+
+//$(document).ready(function () {
+//    var oldCompanies = $.getJSON('http://localhost:8080/api/1/companies');
+//    BuildCompanyTable(oldCompanies);
+//});
 
 function BuildCompanyTable(jsonObj) {
     var companyTable = document.createElement('table');
